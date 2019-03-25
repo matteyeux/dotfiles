c8687a583c925dc8bee65057367d2c3a6b001e2f
mathieu <mathieu.hautebas@gmail.com>
2019-03-25 14:17:35 +0100

    new config

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 
 cp zshrc ~/.zshrc
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cp terminator ~/.config/terminator/config
