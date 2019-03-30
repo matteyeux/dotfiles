a33f8fcde17ec8257e2eb236c75a3c11d1855bd8
mathieu <mathieu.hautebas@gmail.com>
2019-03-30 16:29:09 +0100

    nanorc

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 cp vimrc ~/.vimrc
+cp nanorc ~/.nanorc
 
 # install and setup OH-MY-ZSH
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 cp zshrc ~/.zshrc
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+zsh -c git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 cp terminator ~/.config/terminator/config
