3de863c34713f1edb39c13a4c575b40dd903091d
mathieu <mathieu.hautebas@gmail.com>
2019-04-16 11:40:35 +0200

    update install.sh for my new theme

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
-cp vimrc ~/.vimrc
-cp nanorc ~/.nanorc
+# script to install
+# and setup my dotfiles
+# ~matteyeux
 
 # install and setup OH-MY-ZSH
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 cp zshrc ~/.zshrc
 zsh -c git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# copy my custom theme
+cp matteyeux.zsh-theme ~/.oh-my-zsh/themes/matteyeux.zsh-theme
+
+# install my configs
 cp terminator ~/.config/terminator/config
+cp vimrc ~/.vimrc
+cp nanorc ~/.nanorc
