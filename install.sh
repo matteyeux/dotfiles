35d8ee8416ee78f777179f18348dd2519116741d
mathieu <mathieu.hautebas@gmail.com>
2019-06-03 08:44:35 +0200

    fix bad assignment

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ cp vimrc ~/.vimrc
 cp nanorc ~/.nanorc
 cp tmux.conf ~/.tmux.conf
 
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+#sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 # copy my custom theme
 cp matteyeux.zsh-theme ~/.oh-my-zsh/themes/matteyeux.zsh-theme
