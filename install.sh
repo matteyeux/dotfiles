0f6d809fcec4cc87160c53031c2ccf17037694bd
Mathieu Hautebas <matteyeux@users.noreply.github.com>
2019-06-17 14:10:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cp vimrc ~/.vimrc
 cp nanorc ~/.nanorc
 cp tmux.conf ~/.tmux.conf
 
-#sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 #zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 # copy my custom theme
