a84014563d3907fd9307d50ebe9301c07a667414
mathieu <mathieu.hautebas@gmail.com>
2019-09-30 11:30:38 +0200

    update

--- install.sh
+++ install.sh
@@ -3,15 +3,21 @@
 # and setup my dotfiles
 # ~matteyeux
 
-# install my configs
+# install my config files
 cp zshrc ~/.zshrc
-cp terminator ~/.config/terminator/config
 cp vimrc ~/.vimrc
 cp nanorc ~/.nanorc
 cp tmux.conf ~/.tmux.conf
+cp muttrc ~/.muttrc
 
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-#zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+mkdir -p ~/.config/terminator
+cp terminator ~/.config/terminator/config
 
-# copy my custom theme
-cp matteyeux.zsh-theme ~/.oh-my-zsh/themes/matteyeux.zsh-theme
+# install oh-my-zsh
+# sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# install zsh highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# copy my ZSH custom theme 
+# cp matteyeux.zsh-theme ~/.oh-my-zsh/themes/matteyeux.zsh-theme
